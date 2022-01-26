a161b9c3f12ab2640d3dc660854dcdab85bfc712
Tag-Epic <tagepicuwu@gmail.com>
2022-01-26 20:03:27 +0100

    Fix: Remove unused imports

--- nextcore/http/route.py
+++ nextcore/http/route.py
@@ -25,7 +25,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Any, Literal, Optional
+    from typing import Literal
 
 
 class Route:
