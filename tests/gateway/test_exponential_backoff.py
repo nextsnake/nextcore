7d33cd9a37f2c78d509fb2804527e8d005004757
Tag-Epic <tagepicuwu@gmail.com>
2022-03-03 17:27:29 +0100

    test(gateway): add a test for max in ExponentialBackoff

--- tests/gateway/test_exponential_backoff.py
+++ tests/gateway/test_exponential_backoff.py
@@ -14,3 +14,15 @@ async def test_basic():
         if i == 5:
             break
         i += 1
+
+@mark.asyncio
+@match_time(0.04, 0.01)
+async def test_max():
+    backoff = ExponentialBackoff(0.02, 2, 0.02)
+
+    i = 0
+    async for _ in backoff:
+        if i == 2:
+            break
+        i += 1
+
