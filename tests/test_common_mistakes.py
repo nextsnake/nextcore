0722e037101e96e58b1d00d393391fd2f031501e
Tag-Epic <tagepicuwu@gmail.com>
2022-03-03 22:21:21 +0100

    test: add tests for using get_event_loop and non-relative imports

--- tests/test_common_mistakes.py
+++ tests/test_common_mistakes.py
@@ -1,15 +1,11 @@
 from __future__ import annotations
 
 from pathlib import Path
-from ast import parse, ImportFrom
+from ast import parse, ImportFrom, Import
 
 from typing import Any
 
 def test_bad_typings():
-    """
-    Test that all files have the right license header.
-    """
-
     source_path = Path("nextcore/")
     failures = 0
 
@@ -37,6 +33,49 @@ def test_bad_typings():
                         print(f"{path} has a typing import of {name.name}. Please use {replacement} instead. (Note: This requires from __future__ import annotations)")
                         failures += 1
     assert failures == 0, f"{failures} bad typings found"
+
+def test_using_get_event_loop():
+    source_path = Path("nextcore/")
+    failures = 0
+
+    for path in source_path.rglob("*.py"):
+        with open(path) as f:
+            file_contents = f.read()
+
+            ast = parse(file_contents, filename=str(path))
+            nodes = flatten_ast(ast)
+
+            for node in nodes:
+                if isinstance(node, ImportFrom):
+                    if not node.module == "asyncio":
+                        continue
+
+                    for name in node.names:
+                        if name.name == "get_event_loop":
+                            print(f"{path} uses get_event_loop. Please use get_running_loop instead.")
+                            failures += 1
+    assert failures == 0, f"{failures} uses of get_event_loop found"
+
+def test_use_relative_imports():
+    source_path = Path("nextcore/")
+    failures = 0
+
+    for path in source_path.rglob("*.py"):
+        with open(path) as f:
+            file_contents = f.read()
+
+            ast = parse(file_contents, filename=str(path))
+            nodes = flatten_ast(ast)
+
+            for node in nodes:
+                if not isinstance(node, Import):
+                    continue
+                for name in node.names:
+                    print(f"{path} uses non-relative imports for {name.name}. Please use relative imports instead.")
+                    failures += 1
+    assert failures == 0, f"{failures} uses of relative imports found"
+
+
 
 def flatten_ast(ast: Any) -> list[Any]:
     nodes: list[Any] = []
