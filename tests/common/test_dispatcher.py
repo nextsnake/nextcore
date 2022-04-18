e23fc9c9f931fb04f9bf17379280d6a94153dba0
Tag-Epic <tagepicuwu@gmail.com>
2022-04-18 20:14:47 +0200

    refactor(tests): refactor Dispatcher tests

--- tests/common/test_dispatcher.py
+++ tests/common/test_dispatcher.py
@@ -1,141 +1,180 @@
 from asyncio import TimeoutError as AsyncioTimeoutError
 from asyncio import create_task, get_running_loop, sleep, wait_for, Future
+import logging
 
 from pytest import mark, raises
 
 from nextcore.common.dispatcher import Dispatcher
 
+@mark.asyncio
+async def test_local_sync_listeners():
+    dispatcher: Dispatcher[str] = Dispatcher()
+    
+    got_response: Future[None] = Future()
 
-def test_sync_add_remove_listener():
+    def sync_response_callback() -> None:
+        got_response.set_result(None)
+
+    dispatcher.add_listener(sync_response_callback, "test")
+    await dispatcher.dispatch("test")
+    await wait_for(got_response, timeout=1)
+
+    got_response = Future()
+    dispatcher.remove_listener(sync_response_callback, "test")
+    await dispatcher.dispatch("test")
+
+    with raises(AsyncioTimeoutError):
+        await wait_for(got_response, timeout=.1)
+
+@mark.asyncio
+async def test_global_sync_listeners():
+    dispatcher: Dispatcher[str] = Dispatcher()
+    
+    got_response: Future[None] = Future()
+
+    def sync_response_callback(event_name: str) -> None:
+        del event_name
+        got_response.set_result(None)
+
+    dispatcher.add_listener(sync_response_callback)
+    await dispatcher.dispatch("test")
+    await wait_for(got_response, timeout=1)
+
+    got_response = Future()
+    dispatcher.remove_listener(sync_response_callback)
+    await dispatcher.dispatch("test")
+
+    with raises(AsyncioTimeoutError):
+        await wait_for(got_response, timeout=.1)
+
+@mark.asyncio
+async def test_local_async_listeners():
+    dispatcher: Dispatcher[str] = Dispatcher()
+    
+    got_response: Future[None] = Future()
+
+    async def sync_response_callback() -> None:
+        got_response.set_result(None)
+
+    dispatcher.add_listener(sync_response_callback, "test")
+    await dispatcher.dispatch("test")
+    await wait_for(got_response, timeout=1)
+
+    got_response = Future()
+    dispatcher.remove_listener(sync_response_callback, "test")
+    await dispatcher.dispatch("test")
+
+    with raises(AsyncioTimeoutError):
+        await wait_for(got_response, timeout=.1)
+
+@mark.asyncio
+async def test_global_async_listeners() -> None:
+    dispatcher: Dispatcher[str] = Dispatcher()
+    
+    got_response: Future[None] = Future()
+
+    async def sync_response_callback(event_name: str):
+        del event_name
+        got_response.set_result(None)
+
+    dispatcher.add_listener(sync_response_callback)
+    await dispatcher.dispatch("test")
+    await wait_for(got_response, timeout=1)
+
+    got_response = Future()
+    dispatcher.remove_listener(sync_response_callback)
+    await dispatcher.dispatch("test")
+
+    with raises(AsyncioTimeoutError):
+        await wait_for(got_response, timeout=.1)
+
+
+@mark.asyncio
+async def test_local_error_handler() -> None:
+    dispatcher: Dispatcher[str] = Dispatcher()
+    
+    got_response: Future[None] = Future()
+
+    def error_causer() -> None:
+        raise RuntimeError("Dummy error")
+
+    def error_handler(exception: Exception) -> None:
+        del exception
+        got_response.set_result(None)
+
+    dispatcher.add_listener(error_causer, "test")
+    dispatcher.add_exception_handler(error_handler, "test")
+    await dispatcher.dispatch("test")
+    await wait_for(got_response, timeout=1)
+
+    got_response = Future()
+    dispatcher.remove_listener(error_causer, "test")
+    await dispatcher.dispatch("test")
+
+    with raises(AsyncioTimeoutError):
+        await wait_for(got_response, timeout=.1)
+
+@mark.asyncio
+async def test_global_error_handler() -> None:
+    dispatcher: Dispatcher[str] = Dispatcher()
+    
+    got_response: Future[None] = Future()
+
+    def error_causer() -> None:
+        raise RuntimeError("Dummy error")
+
+    def error_handler(event_name: str, exception: Exception) -> None:
+        del event_name, exception
+        got_response.set_result(None)
+
+    dispatcher.add_listener(error_causer)
+    dispatcher.add_exception_handler(error_handler)
+
+    loop = get_running_loop()
+
+    # Delay the execution so wait_for gets time to run.
+    # TODO: This workaround is bad.
+    loop.call_later(.01, create_task, dispatcher.dispatch("test"))
+
+    await wait_for(got_response, timeout=1)
+
+    got_response = Future()
+    dispatcher.remove_listener(error_causer)
+    loop.call_later(0.01, create_task, dispatcher.dispatch("test"))
+
+    with raises(AsyncioTimeoutError):
+        await wait_for(got_response, timeout=.1)
+
+def test_remove_nonexistant_listener() -> None:
     dispatcher: Dispatcher[str] = Dispatcher()
 
-    def sync_callback():
-        pass
-
-    dispatcher.add_listener(sync_callback, "test")
-    assert len(dispatcher._event_handlers["test"]) == 1, "Sync listener not added"  # type: ignore
-    dispatcher.remove_listener(sync_callback, "test")
-    assert len(dispatcher._event_handlers["test"]) == 0, "Sync listener not removed"  # type: ignore
-
-
-def test_sync_add_remove_global_listener():
-    dispatcher: Dispatcher[str] = Dispatcher()
-
-    def sync_callback():
-        pass
-
-    dispatcher.add_listener(sync_callback)
-    assert len(dispatcher._global_event_handlers) == 1, "Sync listener not added"  # type: ignore
-    dispatcher.remove_listener(sync_callback)
-    assert len(dispatcher._global_event_handlers) == 0, "Sync listener not removed"  # type: ignore
-
-
-def test_remove_listener():
-    dispatcher: Dispatcher[str] = Dispatcher()
-
-    def sync_callback():
+    def sync_callback() -> None:
         pass
 
     with raises(ValueError):
         dispatcher.remove_listener(sync_callback, "test")
 
-
 @mark.asyncio
-async def test_async_add_remove_listener():
+async def test_local_wait_for_handler() -> None:
     dispatcher: Dispatcher[str] = Dispatcher()
+    
+    loop = get_running_loop()
+    loop.call_later(0.01, create_task, dispatcher.dispatch("test"))
+    await wait_for(dispatcher.wait_for(lambda: True, "test"), timeout=0.1)
 
-    async def async_callback():
-        pass
-
-    dispatcher.add_listener(async_callback, "test")
-    assert len(dispatcher._event_handlers["test"]) == 1, "Async listener not added"  # type: ignore
-    dispatcher.remove_listener(async_callback, "test")
-    assert len(dispatcher._event_handlers["test"]) == 0, "Async listener not removed"  # type: ignore
-
-
-@mark.asyncio
-async def test_async_add_remove_global_listener():
-    dispatcher: Dispatcher[str] = Dispatcher()
-
-    async def async_callback():
-        pass
-
-    dispatcher.add_listener(async_callback)
-    assert len(dispatcher._global_event_handlers) == 1, "Async listener not added"  # type: ignore
-    dispatcher.remove_listener(async_callback)
-    assert len(dispatcher._global_event_handlers) == 0, "Async listener not removed"  # type: ignore
-
-
-@mark.asyncio
-async def test_sync_wait_for_no_event():
-    dispatcher: Dispatcher[str] = Dispatcher()
-
+    loop.call_later(0.01, create_task, dispatcher.dispatch("test"))
     with raises(AsyncioTimeoutError):
-        await wait_for(dispatcher.wait_for(lambda: False), timeout=0.1)
-
+        await wait_for(dispatcher.wait_for(lambda: False, "test"), timeout=0.1)
 
 @mark.asyncio
-async def test_async_wait_for_no_event():
+async def test_global_wait_for_handler() -> None:
     dispatcher: Dispatcher[str] = Dispatcher()
+    
+    loop = get_running_loop()
+    loop.call_later(0.01, create_task, dispatcher.dispatch("test"))
+    await wait_for(dispatcher.wait_for(lambda _: True), timeout=0.1)
 
-    async def blank_check():
-        return False
-
+    loop.call_later(0.01, create_task, dispatcher.dispatch("test"))
     with raises(AsyncioTimeoutError):
-        await wait_for(dispatcher.wait_for(blank_check), timeout=0.1)
+        await wait_for(dispatcher.wait_for(lambda _: False), timeout=0.1)
 
-
-@mark.asyncio
-async def test_sync_wait_for_event():
-    dispatcher: Dispatcher[str] = Dispatcher()
-
-    task = create_task(dispatcher.wait_for(lambda: True, "test"))
-    await sleep(0)  # This switches control to the dispatcher to dispatch the event.
-    await dispatcher.dispatch("test")
-    await sleep(0.01)
-
-    assert task.done(), "Task never finished"
-    if exception := task.exception():
-        raise exception
-    assert task.result, "Task finished but no result available"
-
-
-@mark.asyncio
-async def test_async_wait_for_event():
-    dispatcher: Dispatcher[str] = Dispatcher()
-
-    async def blank_check():
-        return True
-
-    task = create_task(dispatcher.wait_for(blank_check, "test"))
-    await sleep(0)  # This switches control to the dispatcher to dispatch the event.
-    await dispatcher.dispatch("test")
-    await sleep(0.01)
-
-    assert task.done(), "Task never finished"
-    if exception := task.exception():
-        raise exception
-    assert task.result, "Task finished but no result available"
-
-
-@mark.asyncio
-async def test_error_handler():
-    dispatcher: Dispatcher[str] = Dispatcher()
-
-    did_error: Future[None] = Future()
-
-    def error_handler(_: Exception) -> None:
-        did_error.set_result(None)
-
-    def error_causer() -> None:
-        raise RuntimeError("Test error!")
-
-    dispatcher.add_exception_handler(error_handler, "test")
-    dispatcher.add_listener(error_causer, "test")
-
-    # Dispatch the event. This is done via call_later as we want to wait for the error handler to run.
-    await dispatcher.dispatch("test")
-
-    await did_error
-
-    assert did_error, "Error flag was not set"
