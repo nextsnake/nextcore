0ef205f691dd167eedbb6bd1a396229ec7ea9586
Tag-Epic <tagepicuwu@gmail.com>
2022-05-09 21:04:58 +0200

    test(http): add gc tests for ratelimit storage

---
+++ tests/http/test_ratelimit_storage.py
@@ -0,0 +1,30 @@
+import gc
+from nextcore.http.ratelimit_storage import RatelimitStorage
+from nextcore.http import Bucket, BucketMetadata
+from pytest import mark
+
+@mark.asyncio
+async def test_does_gc_collect_unused_buckets() -> None:
+    storage = RatelimitStorage()
+
+    metadata = BucketMetadata()
+    bucket = Bucket(metadata)
+
+    await storage.store_bucket_by_nextcore_id(1, bucket)
+    gc.collect()
+
+    assert await storage.get_bucket_by_nextcore_id(1) is None, "Bucket was not collected"
+
+@mark.asyncio
+async def test_does_not_collect_dirty_buckets() -> None:
+    storage = RatelimitStorage()
+
+    metadata = BucketMetadata()
+    bucket = Bucket(metadata)
+
+    await bucket.update(0, 1)
+
+    await storage.store_bucket_by_nextcore_id(1, bucket)
+    gc.collect()
+
+    assert await storage.get_bucket_by_nextcore_id(1) is not None, "Bucket should not be collected"
