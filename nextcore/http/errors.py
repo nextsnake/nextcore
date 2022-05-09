b85d764356e446d1ef86e571cc15cbb04c34b63c
Tag-Epic <tagepicuwu@gmail.com>
2022-05-09 12:21:14 +0200

    fix(http): remove test import

--- nextcore/http/errors.py
+++ nextcore/http/errors.py
@@ -26,7 +26,6 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from aiohttp import ClientResponse
     
-    from discord_typings.shared
     from nextcore.typings.http.error import HTTPError as HTTPErrorTyping # TODO: This doesn't exist in discord_typings yet?
 
 
