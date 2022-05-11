1b3f5497701568690dacf66933ebb531aee92dbb
Tag-Epic <tagepicuwu@gmail.com>
2022-05-11 17:14:39 +0200

    docs: change logo to a transparent svg

--- docs/conf.py
+++ docs/conf.py
@@ -64,5 +64,5 @@ html_theme = "furo"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_logo = "_static/logo.png"
+html_logo = "_static/logo.svg"
 html_favicon = "_static/logo.ico"
