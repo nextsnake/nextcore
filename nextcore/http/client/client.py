ec2da2e7bed3fdc3b3fdd304b373b6fb9c435fc8
ooliver1 <oliverwilkes2006@icloud.com>
2022-07-20 16:32:17 +0100

    refactor(http): move sticker http wrappers

--- nextcore/http/client/client.py
+++ nextcore/http/client/client.py
@@ -37,10 +37,11 @@ from .wrappers import (
     GuildTemplateHTTPWrappers,
     InviteHTTPWrappers,
     StageInstanceHTTPWrappers,
+    StickerHTTPWrappers,
 )
 
 if TYPE_CHECKING:
-    from typing import Any, Final, Literal
+    from typing import Any, Final
 
     from discord_typings import (
         ApplicationData,
@@ -51,8 +52,6 @@ if TYPE_CHECKING:
         GuildMemberData,
         MessageData,
         Snowflake,
-        StickerData,
-        StickerPackData,
         UserData,
         VoiceRegionData,
         WebhookData,
@@ -75,6 +74,7 @@ class HTTPClient(
     GuildTemplateHTTPWrappers,
     InviteHTTPWrappers,
     StageInstanceHTTPWrappers,
+    StickerHTTPWrappers,
     BaseHTTPClient,
 ):
     """The HTTP client to interface with the Discord API.
@@ -181,262 +181,6 @@ class HTTPClient(
     # Invite
     # Stage instance
     # Sticker
-    async def get_sticker(
-        self, authentication: BotAuthentication, sticker_id: Snowflake, *, global_priority: int = 0
-    ) -> StickerData:
-        """Gets a sticker from a sticker id.
-
-        Read the `documentation <https://discord.dev/resources/sticker#get-sticker>`__
-
-        Parameters
-        ----------
-        authentication:
-            Authentication info.
-        sticker_id:
-            The id of the sticker to get.
-        global_priority:
-            The priority of the request for the global rate-limiter.
-
-        Returns
-        -------
-        discord_typings.StickerData
-            The sticker that was fetched
-        """
-        route = Route("GET", "/stickers/{sticker_id}", sticker_id=sticker_id)
-        r = await self._request(
-            route,
-            rate_limit_key=authentication.rate_limit_key,
-            headers={"Authorization": str(authentication)},
-            global_priority=global_priority,
-        )
-
-        # TODO: Make this verify the payload from discord?
-        return await r.json()  # type: ignore [no-any-return]
-
-    async def list_nitro_sticker_packs(
-        self, *, global_priority: int = 0
-    ) -> dict[Literal["sticker_packs"], list[StickerPackData]]:
-        """Gets all nitro sticker packs
-
-        Read the `documentation <https://discord.dev/resources/sticker#list-nitro-sticker-packs>`__
-
-        Parameters
-        ----------
-        global_priority:
-            The priority of the request for the global rate-limiter.
-        """
-        route = Route("GET", "/sticker-packs")
-        r = await self._request(
-            route,
-            rate_limit_key=None,
-            global_priority=global_priority,
-        )
-
-        # TODO: Make this verify the payload from discord?
-        return await r.json()  # type: ignore [no-any-return]
-
-    async def list_guild_stickers(
-        self, authentication: BotAuthentication, guild_id: Snowflake, *, global_priority: int = 0
-    ) -> list[StickerData]:
-        """Gets all custom stickers added by a guild
-
-        Read the `documentation <https://discord.dev/resources/sticker#list-guild-stickers>`__
-
-        .. note::
-            The ``user`` field will be provided if you have the ``MANAGE_EMOJIS_AND_STICKERS`` permission
-
-        Parameters
-        ----------
-        authentication:
-            Authentication info.
-        guild_id:
-            The id of guild to get stickers from
-        global_priority:
-            The priority of the request for the global rate-limiter.
-
-        Returns
-        -------
-        list[discord_typings.StickerData]
-            The stickers in the guild
-        """
-        route = Route("GET", "/guilds/{guild_id}/stickers", guild_id=guild_id)
-
-        r = await self._request(
-            route,
-            rate_limit_key=authentication.rate_limit_key,
-            headers={"Authorization": str(authentication)},
-            global_priority=global_priority,
-        )
-
-        # TODO: Make this verify the payload from discord?
-        return await r.json()  # type: ignore [no-any-return]
-
-    async def get_guild_sticker(
-        self, authentication: BotAuthentication, guild_id: Snowflake, sticker_id: str | int, *, global_priority: int = 0
-    ) -> StickerData:
-        """Get a custom sticker
-
-        Read the `documentation <https://discord.dev/resources/sticker#get-guild-sticker>`__
-
-        .. note::
-            The ``user`` field will be provided if you have the ``MANAGE_EMOJIS_AND_STICKERS`` permission
-
-        Parameters
-        ----------
-        authentication:
-            Authentication info.
-        guild_id:
-            The id of guild to get the sticker from
-        sticker_id:
-            The sticker to get
-        global_priority:
-            The priority of the request for the global rate-limiter.
-
-        Returns
-        -------
-        list[discord_typings.StickerData]
-            The stickers in the guild
-        """
-        route = Route("GET", "/guilds/{guild_id}/stickers/{sticker_id}", guild_id=guild_id, sticker_id=sticker_id)
-
-        r = await self._request(
-            route,
-            rate_limit_key=authentication.rate_limit_key,
-            headers={"Authorization": str(authentication)},
-            global_priority=global_priority,
-        )
-
-        # TODO: Make this verify the payload from discord?
-        return await r.json()  # type: ignore [no-any-return]
-
-    # TODO: Add create guild sticker
-
-    async def modify_guild_sticker(
-        self,
-        authentication: BotAuthentication,
-        guild_id: Snowflake,
-        sticker_id: Snowflake,
-        *,
-        name: str | UndefinedType = UNDEFINED,
-        description: str | UndefinedType = UNDEFINED,
-        tags: list[str] | UndefinedType = UNDEFINED,
-        reason: str | UndefinedType = UNDEFINED,
-        global_priority: int = 0,
-    ) -> StickerData:  # TODO: Make StickerData always include user
-        """Modifies a sticker
-
-        Read the `documentation <https://discord.dev/resources/sticker#modify-guild-sticker>`__
-
-        .. note::
-            This requires the ``MANAGE_EMOJIS_AND_STICKERS`` permission.
-
-        Parameters
-        ----------
-        authentication:
-            Authentication info.
-        guild_id:
-            The id of the guild where the sticker is in.
-        sticker_id:
-            The id of the sticker to update
-        name:
-            The name of the sticker.
-
-            .. note::
-                This has to be between ``2`` and ``30`` characters
-        description:
-            The description of the sticker
-
-            .. note::
-                This has to be between ``2`` and ``100`` characters
-        tags:
-            Autocomplete/suggestion tags for the sticker (max 200 characters)
-        reason:
-            The reason to put in audit log
-        global_priority:
-            The priority of the request for the global rate-limiter.
-
-        Returns
-        -------
-        discord_typings.StageInstanceData
-            The updated stage instance
-        """
-        route = Route("PATCH", "/guilds/{guild_id}/stickers/{sticker_id}", guild_id=guild_id, sticker_id=sticker_id)
-
-        payload: dict[str, Any] = {}
-
-        # These have different behaviour when not provided and set to None.
-        # This only adds them if they are provided (not Undefined)
-        if name is not UNDEFINED:
-            payload["name"] = name
-        if description is not UNDEFINED:
-            payload["description"] = description
-        if tags is not UNDEFINED:
-            payload["tags"] = tags
-
-        headers = {"Authorization": str(authentication)}
-
-        # These have different behaviour when not provided and set to None.
-        # This only adds them if they are provided (not Undefined)
-        if reason is not UNDEFINED:
-            headers["X-Audit-Log-Reason"] = reason
-
-        r = await self._request(
-            route,
-            rate_limit_key=authentication.rate_limit_key,
-            headers=headers,
-            json=payload,
-            global_priority=global_priority,
-        )
-
-        # TODO: Make this verify the payload from discord?
-        return await r.json()  # type: ignore [no-any-return]
-
-    async def delete_guild_sticker(
-        self,
-        authentication: BotAuthentication,
-        guild_id: Snowflake,
-        sticker_id: Snowflake,
-        *,
-        reason: str | UndefinedType = UNDEFINED,
-        global_priority: int = 0,
-    ) -> None:
-        """Modifies a stage instance
-
-        Read the `documentation <https://discord.dev/resources/sticker#delete-guild-sticker>`__
-
-        .. note::
-            This requires the ``MANAGE_CHANNELS``, ``MUTE_MEMBERS`` and ``MOVE_MEMBERS`` permission.
-
-        Parameters
-        ----------
-        authentication:
-            Authentication info.
-        channel_id:
-            The id of the stage channel to delete the stage instance for.
-        reason:
-            The reason to put in audit log
-        global_priority:
-            The priority of the request for the global rate-limiter.
-        """
-        route = Route("DELETE", "/guilds/{guild_id}/stickers/{sticker_id}", guild_id=guild_id, sticker_id=sticker_id)
-
-        headers = {"Authorization": str(authentication)}
-
-        # These have different behaviour when not provided and set to None.
-        # This only adds them if they are provided (not Undefined)
-        if reason is not UNDEFINED:
-            headers["X-Audit-Log-Reason"] = reason
-
-        r = await self._request(
-            route,
-            rate_limit_key=authentication.rate_limit_key,
-            headers=headers,
-            global_priority=global_priority,
-        )
-
-        # TODO: Make this verify the payload from discord?
-        return await r.json()  # type: ignore [no-any-return]
-
     # User
     async def get_current_user(
         self, authentication: BotAuthentication | BearerAuthentication, *, global_priority: int = 0
--- nextcore/http/client/wrappers/__init__.py
+++ nextcore/http/client/wrappers/__init__.py
@@ -28,3 +28,4 @@ from .guild_scheduled_events import *
 from .guild_template import *
 from .invite import *
 from .stage_instance import *
+from .sticker import *
