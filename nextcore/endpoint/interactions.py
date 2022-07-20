f801dc1becb2effa71466654e8f53d9a51b0868e
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2022-07-19 22:50:35 -0500

    Added dispatcher, removed most of the interaction type specific default placeholder methods, changed how interactions are meant to be responded.
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- nextcore/endpoint/interactions.py
+++ nextcore/endpoint/interactions.py
@@ -1,4 +1,27 @@
+# The MIT License (MIT)
+# Copyright (c) 2021-present nextcore developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
 from __future__ import annotations
+
+import asyncio
 
 from aiohttp import web
 from json import JSONDecodeError
@@ -7,36 +30,43 @@ from nacl.exceptions import BadSignatureError
 from nacl.signing import VerifyKey
 from typing import TYPE_CHECKING
 
+from nextcore.common import Dispatcher
+
 
 if TYPE_CHECKING:
+    from typing import Final
+
+    from discord_typings import InteractionData
     from multidict import CIMultiDictProxy
+
 
 
 logger = getLogger(__name__)
 
 
-__all__ = (
+__all__: Final[tuple[str, ...]] = (
     "InteractionEndpoint",
     "verify_signature",
 )
 
 
-def verify_signature(public_key: str, signature: str, timestamp: str, body: str):
+def verify_signature(public_key: str, signature: str, timestamp: str, body: str) -> bool:
     verify_key = VerifyKey(bytes.fromhex(public_key))
     verify_key.verify(f"{timestamp}{body}".encode(), bytes.fromhex(signature))
+    return True
 
 
 class InteractionEndpoint:
     def __init__(
             self,
-            public_key: str,
             *,
             web_app: web.Application = None,
             route_path: str = "/"
     ):
-        self.public_key: str = public_key
+        self.public_key: str | None = None
         self.app: web.Application = web_app or web.Application()
         self.site: web.TCPSite | None = None
+        self.event_dispatcher: Dispatcher = Dispatcher()
 
         self.app.add_routes([
             web.route("POST", route_path, self._on_interaction_incoming)
@@ -44,40 +74,27 @@ class InteractionEndpoint:
 
     async def _verify_from_headers(self, headers: CIMultiDictProxy, body: str) -> bool:
         try:
-            signature = headers.get("x-signature-ed25519")
-            timestamp = headers.get("x-signature-timestamp")
+            signature: str = headers.get("x-signature-ed25519")
+            timestamp: str = headers.get("x-signature-timestamp")
             verify_signature(self.public_key, signature, timestamp, body)
             return True
         except BadSignatureError:
             return False
 
-    async def _on_interaction_incoming(self, request: web.Request) -> web.Response:
+    async def _on_interaction_incoming(self, request: web.Request) -> web.Response | None:
         logger.debug("Incoming interaction endpoint request.")
         try:
             body = (await request.read()).decode("utf-8")
             if await self._verify_from_headers(request.headers, body):
                 logger.debug("Verified Discord signature headers, continuing.")
-                interaction = await request.json()
+                interaction: InteractionData = await request.json()
                 if interaction.get("type") == 1:
-                    logger.debug("Calling on_ping.")
-                    return await self.on_ping(interaction)
-                elif interaction.get("type") == 2:
-                    logger.debug("Calling on_application_command.")
-                    return await self.on_application_command(interaction)
-                elif interaction.get("type") == 3:
-                    logger.debug("Calling on_message_component.")
-                    return await self.on_message_component(interaction)
-                elif interaction.get("type") == 4:
-                    logger.debug("Calling on_application_command_autocomplete.")
-                    return await self.on_application_command_autocomplete(interaction)
-                elif interaction.get("type") == 5:
-                    logger.debug("Calling on_modal_submit.")
-                    return await self.on_modal_submit(interaction)
+                    # logger.debug("Dispatching ping.")
+                    await self.event_dispatcher.dispatch("PING", request, interaction)
                 else:
-                    logger.warning(
-                        "Received interaction of unhandled type %s: %s", interaction.get("type"), interaction
-                    )
+                    await self.event_dispatcher.dispatch("INTERACTION", request, interaction)
 
+                return None
             else:
                 logger.debug("Failed Discord signature headers, returning 401.")
                 return web.Response(body="invalid request signature", status=401)
@@ -86,29 +103,13 @@ class InteractionEndpoint:
             logger.debug("Invalid json given, ignoring: %s", (await request.read()).decode())
             return web.Response(status=400)
 
-    async def on_ping(self, interaction) -> web.Response:
-        return web.json_response(data={"type": 1})
+    async def _handle_ping(self, request: web.Request, interaction):
+        response = web.json_response(data={"type": 1})
+        await response.prepare(request)
+        await response.write_eof()
 
-    async def on_application_command(self, interaction) -> web.Response:
-        return web.json_response(
-            {
-                "type": 4,
-                "data": {
-                    "content": "THIS IS ~~A PLACEHOLDER~~ SPARTA! <https://www.youtube.com/watch?v=PCiIhr8QN1c>"
-                }
-            }
-        )
-
-    async def on_message_component(self, interaction) -> web.Response:
-        pass
-
-    async def on_application_command_autocomplete(self, interaction) -> web.Response:
-        pass
-
-    async def on_modal_submit(self, interaction) -> web.Response:
-        pass
-
-    async def start(self, *, site: web.TCPSite = None, port: int = 8080):
+    async def start(self, public_key: str, *, site: web.TCPSite = None, port: int = 8080):
+        self.public_key = public_key
         if site:
             self.site = site
 
@@ -117,5 +118,22 @@ class InteractionEndpoint:
             await runner.setup()
             self.site = web.TCPSite(runner, "0.0.0.0", port)
 
+        self.event_dispatcher.add_listener(self._handle_ping, "PING")
         await self.site.start()
         logger.info("%s listening on %s", self.__class__.__name__, self.site.name)
+
+    def run(
+            self,
+            public_key: str,
+            *,
+            loop: asyncio.AbstractEventLoop | None = None,
+            site: web.TCPSite | None = None,
+            port: int = 8080
+    ):
+        loop = loop or asyncio.new_event_loop()
+        task = loop.create_task(self.start(public_key, site=site, port=port))
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            logger.debug("KeyboardInterrupt encountered, stopping loop.")
+            task.cancel()
