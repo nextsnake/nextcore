4ba62644c023f9472a4f8d3ac641ea15fbb218c1
TAG-Epic <tagepicuwu@gmail.com>
2022-08-11 21:21:06 +0200

    style: linting... again

--- nextcore/http/bucket.py
+++ nextcore/http/bucket.py
@@ -21,13 +21,14 @@
 
 from __future__ import annotations
 
-from asyncio import PriorityQueue, Event, get_running_loop
+from asyncio import Event, PriorityQueue, get_running_loop
 from contextlib import asynccontextmanager
 from logging import getLogger
 from typing import TYPE_CHECKING, overload
 
-from .request_session import RequestSession
 from nextcore.common.errors import RateLimitedError
+
+from .request_session import RequestSession
 
 if TYPE_CHECKING:
     from typing import AsyncIterator, Final
@@ -52,16 +53,17 @@ class Bucket:
     metadata:
         The metadata for the bucket.
     """
+
     def __init__(self, metadata: BucketMetadata):
         self.metadata: BucketMetadata = metadata
-        self._remaining: int | None = None # None signifies unlimited or not used yet (due to a optimization)
+        self._remaining: int | None = None  # None signifies unlimited or not used yet (due to a optimization)
         self._pending: PriorityQueue[RequestSession] = PriorityQueue()
         self._reserved: list[RequestSession] = []
         self._resetting: bool = False
         self._can_do_blind_request: Event = Event()
 
         self._can_do_blind_request.set()
-    
+
     @asynccontextmanager
     async def acquire(self, *, priority: int = 0, wait: bool = True) -> AsyncIterator[None]:
         """Use a spot in the rate limit.
@@ -88,30 +90,34 @@ class Bucket:
         if self._remaining is None and self.metadata.limit is not None:
             # We have info from metadata! Use that
             self._remaining = self.metadata.limit
-        
+
         if self._remaining is not None:
             # Already using this bucket
 
             session = RequestSession(priority=priority)
 
-            estimated_remaining = self._remaining - len(self._reserved) # We assume every request is successful, and retry when that is not the case.
+            estimated_remaining = self._remaining - len(
+                self._reserved
+            )  # We assume every request is successful, and retry when that is not the case.
 
             if estimated_remaining == 0:
                 if not wait:
                     raise RateLimitedError()
-                self._pending.put_nowait(session) # This can't raise a exception as pending is always infinite unless someone else modified it
-                await session.pending_future # Wait for a spot in the rate limit.
+                self._pending.put_nowait(
+                    session
+                )  # This can't raise a exception as pending is always infinite unless someone else modified it
+                await session.pending_future  # Wait for a spot in the rate limit.
                 # This will automatically be removed by the waker.
 
             self._reserved.append(session)
             try:
-                yield # Let the user do the request
+                yield  # Let the user do the request
             except:
                 # Release one request as we assume the request failed.
                 if self._pending.qsize() > 1:
                     self._release_pending(1)
 
-                raise # Re-raise the exception
+                raise  # Re-raise the exception
             finally:
                 self._reserved.remove(session)
             return
@@ -127,13 +133,13 @@ class Bucket:
 
             self._reserved.append(session)
             try:
-                yield # Let the user do the request
+                yield  # Let the user do the request
             except:
                 # Release one request as we assume the request failed.
                 if self._pending.qsize() > 1:
                     self._release_pending(1)
 
-                raise # Re-raise the exception
+                raise  # Re-raise the exception
             else:
                 if self._remaining is None:
                     logger.warning("A user of Bucket is not calling .update! This will cause performance issues...")
@@ -145,21 +151,27 @@ class Bucket:
                 self._can_do_blind_request.set()
                 logger.debug("Done cleaning up blind request!")
             return
-        
+
         # Currently doing blind request
         await self._can_do_blind_request.wait()
 
         # Try again
         async with self.acquire(priority=priority, wait=wait):
             yield
-                
+
     @overload
     async def update(self, *, unlimited: bool = True) -> None:
         ...
+
     @overload
-    async def update(self, remaining: int | None = None, reset_after: float | None = None, *, unlimited: bool = False) -> None:
+    async def update(
+        self, remaining: int | None = None, reset_after: float | None = None, *, unlimited: bool = False
+    ) -> None:
         ...
-    async def update(self, remaining: int | None = None, reset_after: float | None = None, *, unlimited: bool = False) -> None:
+
+    async def update(
+        self, remaining: int | None = None, reset_after: float | None = None, *, unlimited: bool = False
+    ) -> None:
         if unlimited:
             # Updating metadata is handled by the HTTPClient, so we do not need to do this.
 
@@ -170,7 +182,7 @@ class Bucket:
 
             # Start a reset
             if self._resetting:
-                return # Don't do it when there is already a reset in progress
+                return  # Don't do it when there is already a reset in progress
 
             self._resetting = True
 
@@ -180,8 +192,8 @@ class Bucket:
             loop.call_later(reset_after, self._reset_callback)
 
     def _reset_callback(self) -> None:
-        self._resetting = False # Allow future resets
-        self._remaining = None # It should use metadata's limit as a starting point.
+        self._resetting = False  # Allow future resets
+        self._remaining = None  # It should use metadata's limit as a starting point.
 
         # Reset up to the limit
         self._release_pending(self.metadata.limit)
@@ -193,7 +205,7 @@ class Bucket:
             max_count = min(max_count, self._pending.qsize())
 
         for _ in range(max_count):
-            session = self._pending.get_nowait() # This can't raise a exception due to the guard clause.
+            session = self._pending.get_nowait()  # This can't raise a exception due to the guard clause.
 
             # Mark it as completed in the queue to avoid a infinitly overflowing int
             self._pending.task_done()
@@ -204,12 +216,12 @@ class Bucket:
     def dirty(self) -> bool:
         """Whether the bucket is currently any different from a clean bucket created from a :class:`BucketMetadata`."""
         if self._reserved:
-            return True # Currently doing a request.
+            return True  # Currently doing a request.
 
         if self.metadata.unlimited:
-            return False # Unlimited, following stuff does not matter
+            return False  # Unlimited, following stuff does not matter
 
         if self._remaining is not None:
-            return True # Some edits to remaining has been done
+            return True  # Some edits to remaining has been done
 
         return False
