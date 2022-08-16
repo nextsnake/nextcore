3acae3c9ae57401dcd02fd5f64c7093dd5c4006c
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2022-08-16 18:26:27 -0500

    refactor(int endpoint)! Utilizes a middleware
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- nextcore/endpoint/interactions.py
+++ nextcore/endpoint/interactions.py
@@ -56,26 +56,35 @@ def verify_signature(public_key: str, signature: str, timestamp: str, body: str)
 
 
 class InteractionEndpoint:
-    def __init__(
-            self,
-            *,
-            web_app: web.Application = None,
-            route_path: str = "/"
-    ):
-        self.public_key: str | None = None
-        self.app: web.Application = web_app or web.Application()
-        self.site: web.TCPSite | None = None
-        self.event_dispatcher: Dispatcher = Dispatcher()
+    _public_key: str | None
+    _route: str | None
+    event_dispatcher: Dispatcher
 
-        self.app.add_routes([
-            web.route("POST", route_path, self._on_interaction_incoming)
-        ])
+    # def __init__(
+    #         self,
+    #         *,
+    #         web_app: web.Application = None,
+    #         route_path: str = "/interactions/test"
+    # ):
+    #     self.public_key: str | None = None
+    #     self.app: web.Application = web_app or web.Application(middlewares=[interaction_endpoint("asdf", "/interactions/test")])
+    #     self.site: web.TCPSite | None = None
+    #     self.event_dispatcher: Dispatcher = Dispatcher()
+    #
+    #     self.app.add_routes([
+    #         web.route("POST", route_path, self._on_interaction_incoming)
+    #     ])
 
-    async def _verify_from_headers(self, headers: CIMultiDictProxy, body: str) -> bool:
+    def __init__(self):
+        self._public_key = None
+        self._route = None
+        self.event_dispatcher = Dispatcher()
+
+    def _verify_from_headers(self, headers: CIMultiDictProxy, body: str) -> bool:
         try:
             signature: str = headers.get("x-signature-ed25519")
             timestamp: str = headers.get("x-signature-timestamp")
-            verify_signature(self.public_key, signature, timestamp, body)
+            verify_signature(self._public_key, signature, timestamp, body)
             return True
         except BadSignatureError:
             return False
@@ -84,7 +93,7 @@ class InteractionEndpoint:
         logger.debug("Incoming interaction endpoint request.")
         try:
             body = (await request.read()).decode("utf-8")
-            if await self._verify_from_headers(request.headers, body):
+            if self._verify_from_headers(request.headers, body):
                 logger.debug("Verified Discord signature headers, continuing.")
                 interaction: InteractionData = await request.json()
                 if interaction.get("type") == 1:
@@ -107,30 +116,41 @@ class InteractionEndpoint:
         await response.prepare(request)
         await response.write_eof()
 
-    async def start(self, public_key: str, *, site: web.TCPSite = None, port: int = 8080):
-        self.public_key = public_key
-        if site:
-            self.site = site
+    def middleware(self, public_key: str, route: str = "/interactions"):
+        self._public_key = public_key
+        self._route = route
 
-        if not self.site:
-            runner = web.AppRunner(self.app)
-            await runner.setup()
-            self.site = web.TCPSite(runner, "0.0.0.0", port)
+        @web.middleware
+        async def interaction_endpoint_middleware(request: web.Request, handler):
+            print("MIDDLEWARE TRIGGERED.")
+            if request.path == self._route and request.method == "POST":
+                logger.debug("Request POSTing on interaction endpoint route, intercepting.")
+                return await self._on_interaction_incoming(request)
+            else:
+                resp = await handler(request)
+                return resp
 
+        return interaction_endpoint_middleware
+
+    async def start(self, public_key: str, *, port: int = 8080, route="/interactions"):
+        app = web.Application(middlewares=[self.middleware(public_key, route)])
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.TCPSite(runner, "0.0.0.0", port)
         self.event_dispatcher.add_listener(self._handle_ping, "PING")
-        await self.site.start()
-        logger.info("%s listening on %s", self.__class__.__name__, self.site.name)
+        await site.start()
+        logger.info("%s listening on %s", self.__class__.__name__, site.name)
 
     def run(
             self,
             public_key: str,
             *,
             loop: asyncio.AbstractEventLoop | None = None,
-            site: web.TCPSite | None = None,
-            port: int = 8080
+            port: int = 8080,
+            route: str = "/interactions",
     ):
         loop = loop or asyncio.new_event_loop()
-        task = loop.create_task(self.start(public_key, site=site, port=port))
+        task = loop.create_task(self.start(public_key, port=port, route=route))
         try:
             loop.run_forever()
         except KeyboardInterrupt:
