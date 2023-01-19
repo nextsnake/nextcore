afd3e1aaf84c31c345211169f37f91cbef38ae59
TAG-Epic <tagepicuwu@gmail.com>
2023-01-19 14:47:21 +0100

    test(http): test that unlimited releases requests

--- tests/http/global_rate_limiter/test_unlimited.py
+++ tests/http/global_rate_limiter/test_unlimited.py
@@ -59,3 +59,15 @@ async def test_cancel() -> None:
         async with rate_limiter.acquire():
             raise CancelledError()
     assert len(rate_limiter._pending_requests) == 0, "Pending request was not cleared"  # type: ignore [reportPrivateUsage]
+
+@mark.asyncio
+@match_time(1, .1)
+async def test_reset() -> None:
+    rate_limiter = UnlimitedGlobalRateLimiter()
+
+    rate_limiter.update(1)
+
+    await sleep(.5) # Ensure it registers
+
+    async with rate_limiter.acquire():
+        ...
