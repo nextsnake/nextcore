e2c748c77bd31af17f975caee1133a9b93e61508
TAG-Epic <tagepicuwu@gmail.com>
2023-07-14 01:28:32 +0200

    feat(endpoint): somewhat of a structure for webhook-based responses
    This is not called webhook because of potential confusion between this
    and the channel webhooks.
    
    This is for anything that receives HTTP requests originating from
    Discord's servers.
    
    My structure for this is going to be to
    - Create a adapter per web-framework that sets up a app and HTTP paths,
      and passes the processed data from the request to the main class
    - A main class, unsure of a name that takes the data from the adapters
      and dispatches it and does logic stuff.
    
    Unsure of how the different features like interactions and oauth are
    going to fit into this as there is quite a lot of features to cram into
    one class, however I don't know any better way to structure it.

---
+++ nextcore/endpoint/interactions/primitives.py
@@ -0,0 +1,79 @@
+# The MIT License (MIT)
+# Copyright (c) 2021-present tag-epic
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+from __future__ import annotations
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
+from typing import Final
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+class RequestVerifier:
+    """Helper class to verify requests from Discord
+
+    Parameters
+    ----------
+    public_key:
+        The public key from the developer portal. This can be a hex :class:`str`, or the hex :class:`str` converted to bytes.
+
+        This will be converted to :class:`bytes` under the hood.
+    """
+    def __init__(self, public_key: str | bytes) -> None:
+        if isinstance(public_key, str):
+            public_key = bytes.fromhex(public_key)
+
+        self.verify_key: Final[VerifyKey] = VerifyKey(public_key)
+
+    def check(self, signature: str | bytes, timestamp: str, body: str) -> bool:
+        """Check if a request was made by Discord
+
+        Parameters
+        ----------
+        signature:
+            The signature from the ``X-Signature-Ed25519`` header. This can either be the raw header (a hex encoded :class:`str`) or it converted to :class:`bytes`.
+
+        Returns
+        -------
+        bool
+            If the signature was made from Discord.
+
+            If this is :data:`False` you should reject the request with a ``401`` status code.
+        """
+        
+        if isinstance(signature, str):
+            try:
+                signature = bytes.fromhex(signature)
+            except ValueError:
+                logger.debug("Request did not pass check because of signature not being valid HEX")
+                return False
+
+        payload = f"{timestamp}{body}".encode()
+        try:
+            self.verify_key.verify(payload, signature)
+        except BadSignatureError:
+            logger.debug("Request did not pass check because of verify_key.verify")
+            return False
+        except ValueError:
+            logger.debug("Request did not pass check because of ValueError. This is probably due to wrong length")
+
+        return True
+
